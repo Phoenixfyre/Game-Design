8d64e313044c819667b18086b672c9697ab01094
Phoenixfyre <40770159+Phoenixfyre@users.noreply.github.com>
2018-07-14 22:05:01 -0400

    Update stuff.java

--- stuff.java
+++ stuff.java
@@ -1,8 +1,93 @@
 
-public class stuff {
+				// PUT IN if(pressedkeys.contains(KeyEvent_VK_F) or whatever
 
-	public stuff() {
-		// TODO Auto-generated constructor stub
-	}
+				if(mario.collidesWith(rock1)){
+					
+					rock1.setPosition(mario.getPosition().getX(), mario.getPosition().getY()-10);
+					y2=1;
+					rock1.setVelY(0);
+				}
+				
+				if(mario.collidesWith(rock3)&& rock3.isVisible()){
+					rock3.setPosition(mario.getPosition().getX()+5,mario.getPosition().getY()-10);
+					y3=1;
+					rock3.setVelY(0);
+				}
+				
+				// PUT IN if(!pressedkeys.contains(KeyEvent_VK_F) or whatever
 
-}
+				if(y2==1){
+					rock1.setPosition(mario.getPosition().getX(), mario.getPosition().getY());
+					y2=0;
+					rock1.setVelY(5);
+					}
+					
+					if(y3==1){
+						rock3.setPosition(mario.getPosition().getX(), mario.getPosition().getY()+10);
+						y3=0;
+						rock3.setVelY(5);
+					}
+					
+					
+					
+					
+				//put wherever
+
+					if(rock1.collidesWith(platform1)){
+						rock1.setVelY(0);
+					}
+					if(rock1.collidesWith(platform2)){
+						rock1.setVelY(5);
+					}
+					if(rock1.collidesWith(platform3)){
+						rock1.setVelY(0);
+					}
+					if(rock1.collidesWith(platform4)){
+						rock1.setVelY(0);
+					}
+					if(rock1.getPosition().getY()>1200){
+						rock1.setPosition(platform1.getPosition().getX()+20,platform1.getPosition().getY()-50);
+					}
+					if(rock3.getPosition().getY()>1300){
+						rock3.setPosition(10950,180);
+					}
+					
+					rock1.updatePhys();
+					rock3.updatePhys();
+					
+					if(rock3.collidesWith(platform29)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(platform30)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(movplat1)){
+						rock3.setVelY(5);
+					}
+					if(rock3.collidesWith(movplat2)){
+						rock3.setVelY(5);
+					}
+					if(rock3.collidesWith(movplat3)){
+						rock3.setVelY(5);
+					}
+					if(rock3.collidesWith(movplat4)){
+						rock3.setVelY(5);
+					}
+					if(rock3.collidesWith(platform18)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(platform22)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(platform27)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(platform29)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(pressure2)){
+						rock3.setVelY(0);
+					}
+					if(rock3.collidesWith(platform28)){
+						rock3.setVelY(0);
+					}
